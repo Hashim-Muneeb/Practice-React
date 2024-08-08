c8c495da07bf7749e04924503bd15801c65e884f
Hashim-Muneeb <hashimmuneeb75@gmail.com>
2024-08-08 13:50:04 +0500

    Learning props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './Profile';
 
 function App() {
+
+
+
+  
+  const data = [
+    {
+        State: "Uttar Pradesh",
+        Capital: "Lucknow",
+    },
+    {
+        State: "Gujarat",
+        Capital: "Gandhinagar",
+    },
+    {
+        State: "Karnataka",
+        Capital: "Bengaluru",
+    },
+    {
+        State: "Punjab",
+        Capital: "Chandigarh",
+    },
+    {
+        State: "Maharashtra",
+        Capital: "Mumbai",
+    },
+];
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +46,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Profile state={data[0].State} capital= {data[0].Capital}/>
     </div>
   );
 }
---
+++ src/Profile.jsx
@@ -0,0 +1,8 @@
+function Profile(prop){
+    return(
+        <div>
+        <h1>state =  {prop.state} and its capital = {prop.capital}</h1>
+        </div>
+    )
+}
+export default Profile;
